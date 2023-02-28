178ad42065b8a6c9553ba733b85635f542413d30
Neafye <cowyez@gmail.com>
2023-02-28 20:13:36 -0300

    First commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,24 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+// CTRL+SHIFT+SETA  MUDA A LINHA DE LUGAR NO CÓDIGO
+// CTRL + D DUPLICA A LINHA
+// TABLATURA DO CÓDIGO CTRL+ALT+L
+// SHIFT + 12 PARA MOSTRAR SOMENTE O CÓDIGO
+// CTRL+SHIFT+/  ele seleciona o bloco de comentário
+// CTRL+/ ele comenta linha por linha
+public class PrimeiroPrograma {   //refatorar um nome é no SHIT + F6
+    public static void main(String[] args) {
+        Gato gato = new Gato();  // ALT + shift faz o import
+        System.out.println(gato);
+
+        //Shift+F10 executaria o codigo
+        //CTRL + Y é para deletar linha
+
+
+    /*    int a, b;
+
+        System.out.println("Hello World!");*/
+    }
+}
